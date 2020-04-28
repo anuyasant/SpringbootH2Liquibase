5e39e50c487c6cc2b59f8b5e54efe50d32081bda
anuyasant <sant.anuya@gmail.com>
2020-04-28 15:27:32 +0200

    Initial commit.

---
+++ Springboot-H2DbLiquibase/src/main/java/com/ags/learn/integrate/SpringbootH2DbLiquibase/SpringbootH2DbLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.ags.learn.integrate.SpringbootH2DbLiquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootH2DbLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootH2DbLiquibaseApplication.class, args);
+	}
+
+}
---
+++ Springboot-H2DbLiquibase/src/test/java/com/ags/learn/integrate/SpringbootH2DbLiquibase/SpringbootH2DbLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ags.learn.integrate.SpringbootH2DbLiquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootH2DbLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
